f82200a9c4086e6060f5f8d71e9eae3597b458fa
Musicutd <manuelchircop@gmail.com>
2026-08-15 14:54:47 +0200

    Create page.js

---
+++ app/register/page.js
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  return (
+    <main style={{ maxWidth: 480, margin: "80px auto", padding: 24 }}>
+      <h1>Create your Ruvanas account</h1>
+      <p>Registration will be connected to the platform database next.</p>
+      <a href="/">Back to Ruvanas</a>
+    </main>
+  );
+}
