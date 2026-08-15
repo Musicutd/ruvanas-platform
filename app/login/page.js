878a0c18de957cd2561e72472890a62634d93444
Musicutd <manuelchircop@gmail.com>
2026-08-15 14:54:01 +0200

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <main style={{ maxWidth: 480, margin: "80px auto", padding: 24 }}>
+      <h1>Client login</h1>
+      <p>The Ruvanas client portal will be available here.</p>
+      <a href="/">Back to Ruvanas</a>
+    </main>
+  );
+}
