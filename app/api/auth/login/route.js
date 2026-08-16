ec94a5e394dd8c295554bf4956d48fccd674de29
Musicutd <manuelchircop@gmail.com>
2026-08-16 17:59:33 +0200

    Update route.js

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -43,7 +43,8 @@ export async function POST(request) {
       user: {
         id: user.id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        role: user.role
       }
     });
   } catch (error) {
